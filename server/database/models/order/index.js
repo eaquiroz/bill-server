feb6bb964f1ae8904c82b558e90c645978a2c986
Eddie Quiroz <eddie@nexweb.com>
2021-03-15 16:31:45 -0400

    Orders change
    Orders change

--- server/database/models/order/index.js
+++ server/database/models/order/index.js
@@ -8,9 +8,9 @@ const {TaxModel} = require('../index');
 const OrderItemSchema = new mongoose.Schema({
     refId: { type: objectId, ref: 'Item'},
     itemId: { type: Number},
-    quantity: { type: String, required: true },
-    itemPrice: { type: String, required: true },
-    itemTotal: { type: String}
+    quantity: { type: Number, required: true },
+    itemPrice: { type: Number, required: true },
+    itemTotal: { type: Number}
 }, { versionKey: false });
 
 const OrderSchema = new mongoose.Schema({
@@ -18,9 +18,9 @@ const OrderSchema = new mongoose.Schema({
     name: { type: String, required: true },
     orderNumber: { type: Number },
     email: { type: String, required: true },
-    total: { type: String, required: true },
+    total: { type: Number },
     type: { type: String, enum:['drinks','combo'] },
-    serviceTax: {type: String},
+    serviceTax: {type: Number},
     invoice: {type: String}
 }, { versionKey: false });
 
@@ -31,23 +31,38 @@ let getTotal = function (items){
     return sum;
 }
 
+// OrderItemSchema.pre('save', async function (next) {
+//
+//     let items  = this.items;
+//     console.log(items)
+//     items= items.map((data)=>{
+//        return  data.itemTotal = data.quantity * data.price;
+//     })
+//     next();
+// });
+
 OrderSchema.pre('save', async function (next) {
 
     let items  = this.items;
-    let orderType ='drink';
-    items && items.length >0  && items.forEach((data)=>{
+    let orderType ='drinks';
+     items.forEach((data)=>{
         if(data.itemId === 3 || data.itemId ===4){
             orderType = 'combo';
         }
-        data.itemTotal = data.quantity * data.price;
-    })
+          data.itemTotal = data.quantity * data.itemPrice;
+     })
 
-    let tax = await mongoose.models['TaxModel'].findOne({orderType: orderType});
-
-    if (tax) {
+    let tax = await mongoose.models['Tax'].findOne({orderType: orderType},{tax:1});
+        this.type = orderType;
+        console.log(tax,"tax")
+    if (tax.tax) {
         this.serviceTax = tax.tax;
         let total = getTotal(this.items) ;
-        this.total = total + (total*100)/10;
+        this.total = total + (Number(total*10)/100);
+    }else {
+        this.serviceTax = tax.tax;
+        let total = getTotal(this.items) ;
+        this.total = total;
     }
     next();
 });
